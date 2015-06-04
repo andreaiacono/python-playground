a96750f33683f933e5e20bdf2749f94ccd08aa10
Andrea Iacono <andrea.iacono.nl@gmail.com>
2015-06-04 23:40:38 +0200

    old local changes

--- sequence_finder/finder.py
+++ sequence_finder/finder.py
@@ -1,15 +1,24 @@
 import random
 from string import Template
 
+from cStringIO import StringIO
+import sys
 
-OPERATORS = ['+=', '-=', '*=', '/=', '^n']
+
+OPERATORS = ['+=', '-=', '*=', '^=']
 INPUT_NAME = 'input'
 TEMPLATE = Template('''
 #import math
 
-def compute(''' + INPUT_NAME + ''', output):
+def compute(''' + INPUT_NAME + '''):
 $code
 \treturn ''' + INPUT_NAME + '''
+
+def fit(input, output):
+    return input - output
+
+#print(compute($input))
+print(fit(compute($input), $output))
 
 ''')
 
@@ -19,20 +28,47 @@ def get_operator():
 
 
 def get_operand():
-    return str(random.randint(0, 10))
+
+
+    if random.randint(0, 3) > 1:
+        return str(random.randint(0, 10))
+
+    return INPUT_NAME;
+
+
 
 
 def get_new_program():
     code = ''
-    for line in range(random.randint(0, 5)):
+    for line in range(random.randint(1, 5)):
         code += '\t' + INPUT_NAME + ' ' + str(get_operator()) + ' ' + str(get_operand()) + '\n'
-    return TEMPLATE.substitute(code=code)
+    return code
+
 
 # program = get_new_program()
 # print(program)
 # print(exec(program))
 
+def print_result(vals, code):
+    return '<h2>Sequence finder</h2><br/>Requested sequence: ' + str(vals) + '</br>Code: <pre>' + str(code) + '</pre>'
 
 def execute(values):
+    print("starting")
     vals = map(int, values.split(","))
-    return "execution result: " + str(vals[2])
+
+    result = 1
+    counter = 0
+    while result != 0:
+        counter += 1
+        code = get_new_program()
+        print("Try: " + str(counter) + " CODE\n" + code)
+        for i in range(len(vals)):
+            old_stdout = sys.stdout
+            redirected_output = sys.stdout = StringIO()
+            exec(TEMPLATE.substitute(code=code, input=vals[i], output=str(i+1)))
+            sys.stdout = old_stdout
+            result += int(redirected_output.getvalue().strip())
+
+    if result == 0:
+        return print_result(vals, code)
+
