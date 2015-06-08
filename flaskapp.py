6655b9cedf3ae3dc3bab0b481a77a6cfc8bfb40c
andrea <a.iacono@disbrain.com>
2015-06-08 14:19:41 +0200

    fixed math sample

--- flaskapp.py
+++ flaskapp.py
@@ -17,7 +17,7 @@ def serveStaticResource(resource):
 
 @app.route("/test")
 def test():
-    return render_template("sequence_finder/math_sample.html")
+    return render_template("sequence_finder/math.html")
 
 
 @app.route("/finder", methods=[ 'GET'])
